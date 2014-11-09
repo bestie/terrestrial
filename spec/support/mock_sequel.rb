cfc9bb6415de25c80346ad9b1f6c015c50ed6f1f
Stephen Best <bestie@gmail.com>
2014-11-09 15:36:14 +0000

    Insert and update for MockSequel::Relation

--- spec/support/mock_sequel.rb
+++ spec/support/mock_sequel.rb
@@ -1,39 +1,78 @@
 class SequelMapper::MockSequel
-  def initialize(relations)
-    @relations = relations
+  def initialize(relation_data)
+    @relations = Hash[
+      relation_data.map { |table_name, rows|
+        [table_name, Relation.new(rows)]
+      }
+    ]
   end
 
+  attr_reader :relations
+  private     :relations
+
   def [](table_name)
-    Relation.new(@relations.fetch(table_name))
+    @relations.fetch(table_name)
+  end
+
+  class Query
+    def initialize(criteria = {}, &block)
+      if block
+        raise NotImplementedError.new("Block filtering not implemented")
+      end
+      @criteria = criteria
+    end
+
+    attr_reader :criteria
   end
 
   class Relation
     include Enumerable
 
-    def initialize(rows)
+    def initialize(rows, applied_query: Query.new)
       @rows = rows
+      @applied_query = applied_query
     end
 
-    def where(criteria, &block)
-      if block
-        raise NotImplementedError.new("Block filtering not implemented")
-      end
+    attr_reader :rows, :applied_query
+    private     :rows, :applied_query
 
-      self.class.new(equality_filter(criteria))
+    def where(criteria, &block)
+      query = Query.new(criteria, &block)
+      self.class.new(rows, applied_query: query)
     end
 
     def to_a
-      @rows
+      matching_rows
     end
 
     def each(&block)
-      to_a.each(&block)
+      matching_rows.each(&block)
+    end
+
+    def insert(rows)
+      Array(rows).each do |row|
+        canonical.rows.push(row)
+      end
+    end
+
+    def update(attrs)
+      # No need to get the rows from the canonical relation as the hashes can
+      # just be mutated in plaace.
+      matching_rows.each do |row|
+        attrs.each do |k, v|
+          row[k] = v
+        end
+      end
     end
 
     private
 
+    def matching_rows
+      equality_filter(applied_query.criteria)
+    end
+
     def equality_filter(criteria)
-      @rows.select { |row|
+      rows.select { |row|
         criteria.all? { |k, v|
           if v.is_a?(Enumerable)
             v.include?(row.fetch(k))
