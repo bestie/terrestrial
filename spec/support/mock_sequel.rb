b715bda65cc5c8cd47157bacfa667a40f99acb22
Stephen Best <bestie@gmail.com>
2014-11-13 10:41:31 +0000

    More features for the Sequel mock

--- spec/support/mock_sequel.rb
+++ spec/support/mock_sequel.rb
@@ -15,30 +15,65 @@ class SequelMapper::MockSequel
   end
 
   class Query
-    def initialize(criteria = {}, &block)
+    def initialize(criteria: {}, order: [], reverse: false, &block)
       if block
         raise NotImplementedError.new("Block filtering not implemented")
       end
+
       @criteria = criteria
+      @order_columns = order
+      @reverse_order = reverse
     end
 
-    attr_reader :criteria
+    attr_reader :criteria, :order_columns
+
+    def where(new_criteria, &block)
+      self.class.new(
+        criteria: criteria.merge(new_criteria),
+        order: order,
+        reverse: reverse,
+        &block
+      )
+    end
+
+    def order(columns)
+      self.class.new(
+        criteria: criteria,
+        order: columns,
+      )
+    end
+
+    def reverse
+      self.class.new(
+        criteria: criteria,
+        order: order_columns,
+        reverse: true,
+      )
+    end
+
+    def reverse_order?
+      !!@reverse_order
+    end
   end
 
   class Relation
     include Enumerable
 
-    def initialize(rows, applied_query: Query.new)
-      @rows = rows
+    def initialize(all_rows, applied_query: Query.new)
+      @all_rows = all_rows
       @applied_query = applied_query
     end
 
-    attr_reader :rows, :applied_query
-    private     :rows, :applied_query
+    attr_reader :all_rows, :applied_query
+    private     :all_rows, :applied_query
 
     def where(criteria, &block)
-      query = Query.new(criteria, &block)
-      self.class.new(rows, applied_query: query)
+      query = Query.new(criteria: criteria, &block)
+      self.class.new(all_rows, applied_query: query)
+    end
+
+    def order(columns)
+      self.class.new(all_rows, applied_query: query)
     end
 
     def to_a
@@ -51,12 +86,12 @@ class SequelMapper::MockSequel
 
     def delete
       matching_rows.each do |row_to_delete|
-        rows.delete(row_to_delete)
+        all_rows.delete(row_to_delete)
       end
     end
 
     def insert(new_row)
-      rows.push(new_row)
+      all_rows.push(new_row)
     end
 
     def update(attrs)
@@ -76,10 +111,23 @@ class SequelMapper::MockSequel
     private
 
     def matching_rows
-      equality_filter(applied_query.criteria)
+      apply_sort(
+        equality_filter(all_rows, applied_query.criteria),
+        applied_query.order_columns,
+        applied_query.reverse_order?,
+      )
     end
 
-    def equality_filter(criteria)
+    def apply_sort(rows, order_columns, reverse_order)
+      rows
+      sorted_rows = rows.sort_by{ |row|
+        order_columns.map { |col| row.fetch(col) }
+      }
+
+      reverse_order ? sorted_rows.reverse : sorted_rows
+    end
+
+    def equality_filter(rows, criteria)
       rows.select { |row|
         criteria.all? { |k, v|
           if v.is_a?(Enumerable)
