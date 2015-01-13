52cb8c3606578e2e502163b9971947f1043f0dfc
Mike Patrick <mike63443@gmail.com>
2015-01-13 14:54:53 +0000

    Put QuerableLazyDatasetLoader in namespace module

--- lib/sequel_mapper/queryable_lazy_dataset_loader.rb
+++ lib/sequel_mapper/queryable_lazy_dataset_loader.rb
@@ -1,31 +1,33 @@
 require "forwardable"
 
-class QueryableLazyDatasetLoader
-  extend Forwardable
-  include Enumerable
+module SequelMapper
+  class QueryableLazyDatasetLoader
+    extend Forwardable
+    include Enumerable
 
-  def initialize(database_enum, loader, association_mapper = :mapper_not_provided)
-    @database_enum = database_enum
-    @loader = loader
-    @association_mapper = association_mapper
-  end
+    def initialize(database_enum, loader, association_mapper = :mapper_not_provided)
+      @database_enum = database_enum
+      @loader = loader
+      @association_mapper = association_mapper
+    end
 
-  attr_reader :database_enum, :loader
-  private     :database_enum, :loader
+    attr_reader :database_enum, :loader
+    private     :database_enum, :loader
 
-  def_delegators :database_enum, :where
+    def_delegators :database_enum, :where
 
-  def eager_load(association_name)
-    @association_mapper.eager_load_association(database_enum, association_name)
-  end
+    def eager_load(association_name)
+      @association_mapper.eager_load_association(database_enum, association_name)
+    end
 
-  def where(criteria)
-    self.class.new(database_enum.where(criteria), loader)
-  end
+    def where(criteria)
+      self.class.new(database_enum.where(criteria), loader)
+    end
 
-  def each(&block)
-    database_enum
-      .map(&loader)
-      .each(&block)
+    def each(&block)
+      database_enum
+        .map(&loader)
+        .each(&block)
+    end
   end
 end
