be539e951d99ec5e032f9699a6dd5e05230a974d
Stephen Best <bestie@gmail.com>
2015-01-14 00:48:14 +0000

    Refactor RootMapper to fluid interface

--- lib/sequel_mapper/root_mapper.rb
+++ lib/sequel_mapper/root_mapper.rb
@@ -3,6 +3,7 @@ require "sequel_mapper/mapper_methods"
 module SequelMapper
   class RootMapper
     include MapperMethods
+    include Enumerable
 
     def initialize(relation:, mapping:, dirty_map:)
       @relation = relation
@@ -13,14 +14,30 @@ module SequelMapper
     attr_reader :relation, :mapping
     private     :relation, :mapping
 
-    def where(criteria)
+    def each(&block)
       relation
-        .where(criteria)
         .map(&row_loader_func)
+        .each(&block)
+    end
+
+    def where(criteria)
+      new_with_dataset(
+        relation.where(criteria)
+      )
     end
 
     def save(graph_root)
       upsert_if_dirty(mapping.dump(graph_root))
+    end
+
+    private
+
+    def new_with_dataset(dataset)
+      self.class.new(
+        relation: dataset,
+        mapping: mapping,
+        dirty_map: dirty_map,
+      )
     end
   end
 end
