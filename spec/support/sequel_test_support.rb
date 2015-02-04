ef06ea46ae5531c36dd7fec5c38a9fec6751adf3
Stephen Best <bestie@gmail.com>
2015-02-04 23:06:11 +0000

    Query counter to removes describe queries from read counts
    Auto configuration relies on describe queries

--- spec/support/sequel_test_support.rb
+++ spec/support/sequel_test_support.rb
@@ -22,6 +22,12 @@ module SequelMapper
       end
 
       def read_count
+        read_count_with_describes -
+          list_tables_query_count -
+          describe_table_queries_count
+      end
+
+      def read_count_with_describes
         @info.count { |query|
           /\A\([0-9\.]+s\) SELECT/i === query
         }
@@ -50,9 +56,51 @@ module SequelMapper
       end
 
       def reset
+        @described_table_queries = []
         @info = []
         @error = []
         @warn = []
+      end
+
+      private
+
+      def list_tables_query_count
+        @info.count { |query| list_tables_query_pattern.match(query) }
+      end
+
+      def describe_table_queries_count
+        describe_table_queries.count
+      end
+
+      def describe_table_queries
+        # TODO this could probably be better solved with finite automata
+        described_table_queries = []
+
+        queries_without_table_list
+          .take_while { |query|
+            described_table_queries.push(query)
+            described_table_query_pattern.match(query) &&
+              described_table_queries.length == described_table_queries.uniq.length
+          }
+      end
+
+      def queries_without_table_list
+        @info
+          .drop_while { |query|
+            !list_tables_query_pattern.match(query)
+          }
+          .drop_while { |query|
+            list_tables_query_pattern.match(query)
+          }
+      end
+
+
+      def list_tables_query_pattern
+        /\A\([0-9\.]+s\) SELECT "relname" FROM "pg_class"/
+      end
+
+      def described_table_query_pattern
+        /\A\([0-9\.]+s\) SELECT \* FROM "[^"]+" LIMIT 1/i
       end
     end
   end
