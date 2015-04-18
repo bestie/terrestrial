fb450c51ca99122c1f66f0d4ee76df84851c8087
Stephen Best <bestie@gmail.com>
2015-04-18 17:56:43 +0100

    End to end new objects -> database journey

--- lib/sequel_mapper/mapper.rb
+++ lib/sequel_mapper/mapper.rb
@@ -2,9 +2,11 @@ require "sequel_mapper/namespaced_record"
 
 module SequelMapper
   class Mapper
-    def initialize(namespace:, factory:, serializer:, fields:, associations:, dataset:, mappers:)
+    def initialize(datastore:, mapping_name:, namespace:, primary_key:, serializer:, fields:, associations:, dataset:, mappers:)
+      @datastore = datastore
+      @mapping_name = mapping_name
       @namespace = namespace
-      @factory = factory
+      @primary_key = primary_key
       @serializer = serializer
       @fields = fields
       @associations = associations
@@ -12,23 +14,31 @@ module SequelMapper
       @mappers = mappers
     end
 
-    attr_reader :namespace, :factory, :serializer, :fields, :associations, :dataset, :mappers
-    private     :namespace, :factory, :serializer, :fields, :associations, :dataset, :mappers
+    attr_reader :datastore
+    private     :datastore
 
-    def where(criteria)
-      dataset
-        .where(criteria)
-        .map { |row| factory.call(row) }
+    # For serialization
+    attr_reader :mapping_name, :namespace, :primary_key, :serializer, :fields, :associations, :dataset, :mappers
+    private     :mapping_name, :namespace, :primary_key, :serializer, :fields, :associations, :dataset, :mappers
+
+    def save(graph)
+      dump(graph).each do |record|
+        upsert(namespace, record)
+      end
+
+      self
     end
 
+    # TODO Move into another object, GraphSerializer perhaps
     def dump(object, foreign_key = {}, stack = [])
       serialized_record = serializer.call(object)
 
       current_record = NamespacedRecord.new(
         namespace,
+        record_identity(serialized_record),
         serialized_record
           .select { |k, _v| fields.include?(k) }
-          .merge(foreign_key)
+          .merge(foreign_key),
       )
 
       if stack.include?(current_record)
@@ -50,6 +60,19 @@ module SequelMapper
           raise "Association type not supported"
         end
       }
+    end
+
+    private
+
+    def upsert(namespace, record)
+      # TODO the record must also carry its primary key field name
+      existing = datastore[record.namespace].where(record.identity)
+
+      if existing.empty?
+        datastore[record.namespace].insert(record.to_h)
+      else
+        existing.update(record.to_h)
+      end
     end
 
     # TODO: remove use of stack.last, doesn't communicate meaning very well
@@ -88,15 +111,21 @@ module SequelMapper
           this_record,
           NamespacedRecord.new(
             config.fetch(:through_namespace),
-            config.fetch(:foreign_key) => stack.last.fetch(config.fetch(:key)),
-            config.fetch(:association_foreign_key) => this_record.fetch(config.fetch(:association_key)),
+            {
+              config.fetch(:foreign_key) => stack.last.fetch(config.fetch(:key)),
+              config.fetch(:association_foreign_key) => this_record.fetch(config.fetch(:association_key)),
+            },
           ),
         ]
       }.flatten(1)
     end
 
-    def mapping
-      @mapping ||= mappers.fetch(mapping_name)
+    def record_identity(record)
+      Hash[
+        primary_key.map { |field|
+          [field, record.fetch(field)]
+        }
+      ]
     end
   end
 end
---
+++ spec/new_graph_persistence_spec.rb
@@ -0,0 +1,75 @@
+require "spec_helper"
+require "support/mapper_setup"
+require "support/sequel_persistence_setup"
+require "support/have_persisted_matcher"
+
+RSpec.describe "Persist a new graph in empty datastore" do
+  include_context "mapper setup"
+  include_context "sequel persistence setup"
+
+  let(:user_mapper) { mappers[:users] }
+
+  before do
+    truncate_tables
+  end
+
+  context "given a graph of new objects" do
+    it "persists the root node" do
+      user_mapper.save(hansel)
+
+      expect(datastore).to have_persisted(:users, {
+        id: hansel.id,
+        first_name: hansel.first_name,
+        last_name: hansel.last_name,
+        email: hansel.email,
+      })
+    end
+
+    it "persists one to many related nodes 1 level deep" do
+      user_mapper.save(hansel)
+
+      expect(datastore).to have_persisted(:posts, {
+        id: "posts/1",
+        subject: "Biscuits",
+        body: "I like them",
+        author_id: "users/1",
+      })
+
+      expect(datastore).to have_persisted(:posts, {
+        id: "posts/2",
+        subject: "Sleeping",
+        body: "I do it three times purrr day",
+        author_id: "users/1",
+      })
+    end
+
+    it "persists one to many related nodes 2 levels deep" do
+      user_mapper.save(hansel)
+
+      expect(datastore).to have_persisted(:comments, {
+        id: "comments/1",
+        body: "oh noes",
+        post_id: "posts/1",
+        commenter_id: "users/1",
+      })
+    end
+
+    it "persists many to many related nodes" do
+      user_mapper.save(hansel)
+
+      expect(datastore).to have_persisted(:categories, {
+        id: "categories/1",
+        name: "Cat biscuits",
+      })
+    end
+
+    it "persists a 'join table' to faciliate many to many" do
+      user_mapper.save(hansel)
+
+      expect(datastore).to have_persisted(:categories_to_posts, {
+        category_id: "categories/1",
+        post_id: "posts/1",
+      })
+    end
+  end
+end
