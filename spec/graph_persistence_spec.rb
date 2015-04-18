14bb7cb88f999e15d1801fd1f8642b014b9ce0d0
Stephen Best <bestie@gmail.com>
2015-04-18 17:55:45 +0100

    Move have_persisted matcher to own file

--- spec/graph_persistence_spec.rb
+++ spec/graph_persistence_spec.rb
@@ -296,24 +296,4 @@ RSpec.describe "Graph persistence" do
       ))
     end
   end
-
-  RSpec::Matchers.define :have_persisted do |relation_name, data|
-    match do |datastore|
-      datastore[relation_name].find { |record|
-        if data.respond_to?(:===)
-          data === record
-        else
-          data == record
-        end
-      }
-    end
-
-    failure_message do |datastore|
-      "expected #{datastore[relation_name]} to have persisted #{data.inspect} in #{relation_name}"
-    end
-
-    failure_message_when_negated do |datastore|
-      failure_message.gsub("to have", "not to have")
-    end
-  end
 end
