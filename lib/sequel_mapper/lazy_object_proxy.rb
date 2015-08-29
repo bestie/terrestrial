5e14016cbee3983a0a28b78faa1ddf3e979826b4
Stephen Best <bestie@gmail.com>
2015-08-29 21:13:19 +0100

    Consistent namespacing of classes

--- lib/sequel_mapper/lazy_object_proxy.rb
+++ lib/sequel_mapper/lazy_object_proxy.rb
@@ -1,42 +1,42 @@
-SequelMapper ||= Module.new
-
-class SequelMapper::LazyObjectProxy
-  def initialize(object_loader, known_fields)
-    @object_loader = object_loader
-    @known_fields = known_fields
-    @lazy_object = nil
-  end
-
-  attr_reader :object_loader, :known_fields
-  private     :object_loader, :known_fields
-
-  def method_missing(method_id, *args, &block)
-    if args.empty? && known_fields.include?(method_id)
-      known_fields.fetch(method_id)
-    else
-      lazy_object.public_send(method_id, *args, &block)
+module SequelMapper
+  class LazyObjectProxy
+    def initialize(object_loader, known_fields)
+      @object_loader = object_loader
+      @known_fields = known_fields
+      @lazy_object = nil
     end
-  end
 
-  def loaded?
-    !!@lazy_object
-  end
+    attr_reader :object_loader, :known_fields
+    private     :object_loader, :known_fields
 
-  def __getobj__
-    lazy_object
-  end
+    def method_missing(method_id, *args, &block)
+      if args.empty? && known_fields.include?(method_id)
+        known_fields.fetch(method_id)
+      else
+        lazy_object.public_send(method_id, *args, &block)
+      end
+    end
 
-  def each_loaded(&block)
-    [self].select(&:loaded?).each(&block)
-  end
+    def loaded?
+      !!@lazy_object
+    end
 
-  private
+    def __getobj__
+      lazy_object
+    end
 
-  def respond_to_missing?(method_id, _include_private = false)
-    known_fields.include?(method_id) || lazy_object.respond_to?(method_id)
-  end
+    def each_loaded(&block)
+      [self].select(&:loaded?).each(&block)
+    end
 
-  def lazy_object
-    @lazy_object ||= object_loader.call
+    private
+
+    def respond_to_missing?(method_id, _include_private = false)
+      known_fields.include?(method_id) || lazy_object.respond_to?(method_id)
+    end
+
+    def lazy_object
+      @lazy_object ||= object_loader.call
+    end
   end
 end
