9b80246e4b67d748db44b4ed51c40d376ebafa48
Stephen Best <bestie@gmail.com>
2015-09-24 18:07:00 +0100

    Remove unused code from rewrite

--- lib/sequel_mapper.rb
+++ lib/sequel_mapper.rb
@@ -26,25 +26,6 @@ module SequelMapper
 
   private
 
-  def config_to_mappings(config)
-    Hash[
-      configs.map { |name, config|
-        [
-          name,
-          SequelMapper::RelationMapping.new(
-            name: name,
-            namespace: config.fetch(:namespace),
-            fields: config.fetch(:fields),
-            primary_key: config.fetch(:primary_key),
-            serializer: serializer.call(config.fetch(:fields) + config.fetch(:associations).keys),
-            associations: config.fetch(:associations),
-            factory: config.fetch(factory),
-          )
-        ]
-      }
-    ]
-  end
-
   class DirtyMap
     def initialize(storage)
       @storage = storage
@@ -93,41 +74,6 @@ module SequelMapper
 
     def hash_key(record)
       [record.namespace, record.identity]
-    end
-  end
-
-  require "fetchable"
-  class MapperRegistry
-    include Fetchable
-
-    def initialize(mapper_factory:, datastore:, config:, dirty_map:)
-      @mapper_factory = mapper_factory
-      @datastore = datastore
-      @config = config
-      @dirty_map = dirty_map
-
-      @mappers = {}
-    end
-
-    attr_reader :mapper_factory, :datastore, :config, :dirty_map, :mappers
-    private     :mapper_factory, :datastore, :config, :dirty_map, :mappers
-
-    def [](name)
-      mappers.fetch(name) {
-        mappers.store(name, create_mapper(name))
-      }
-    end
-
-    alias_method :from, :[]
-
-    def create_mapper(name)
-      mapping = config.fetch(name)
-
-      mapper_factory.call(
-        relation: datastore[mapping.relation_name],
-        mapping: mapping,
-        dirty_map: dirty_map,
-      )
     end
   end
 end
