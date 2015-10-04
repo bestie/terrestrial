4f2235f835f6a698771c15beaa9d7a07e058069b
Stephen Best <bestie@gmail.com>
2015-10-04 11:59:46 +0200

    Tidy up example spec runner

--- spec/readme_examples_spec.rb
+++ spec/readme_examples_spec.rb
@@ -14,32 +14,22 @@ require "sequel_mapper"
 
 RSpec.describe "README examples" do
   include_context "sequel persistence setup"
-  include_context "seed data setup"
 
   readme_contents = File.read("README.md")
-
-  convenience_id_mappings = {
-    "2f0f791c-47cf-4a00-8676-e582075bcd65" => "users/1",
-    "9b75fe2b-d694-4b90-9137-6201d426dda2" => "posts/1",
-    "bd564cc0-b8f1-45e6-9287-1ae75878c665" => "posts/2",
-    "4af129d0-5b9f-473e-b35d-ae0125a4f79e" => "posts/3",
-  }
 
   code_samples = readme_contents
     .split("```ruby")
     .drop(1)
     .map { |s| s.split("```").first }
-    .map { |s| s.gsub(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, convenience_id_mappings) }
 
-  code_samples.take(1).each do |code_sample|
-    it "executes" do
-      File.open("./example1.rb", "w") { |f| f.puts(code_sample) }
-
-    begin
-      Module.new.module_eval(code_sample)
-    rescue => e
-      binding.pry
-    end
+  code_samples.each_with_index do |code_sample, i|
+    it "executes without error" do
+      begin
+        Module.new.module_eval(code_sample)
+      rescue => e
+        File.open("./example#{i}.rb", "w") { |f| f.puts(code_sample) }
+        binding.pry if ENV["DEBUG"]
+      end
     end
   end
 end
